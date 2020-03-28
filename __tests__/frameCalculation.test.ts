a1f84ee79b3445f3d39e51edab24f3150355af08
tiwuofficial <theimaginationwasunbroken@gmail.com>
2020-03-28 18:46:46 +0900

    wip

--- __tests__/frameCalculation.test.ts
+++ __tests__/frameCalculation.test.ts
@@ -1,7 +1,7 @@
 import { frameCalculation } from '../src/module/frameCalculation';
 
 describe('frameCalculation', (): void => {
-  test('frameCalculation', (): void => {
+  test('frameCalculation ', (): void => {
     const result: number = frameCalculation("1");
     expect(result).toBe(1);
   });
