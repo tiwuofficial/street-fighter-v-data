d1aa759d748ae0c7690d5be763b606e055084000
tiwuofficial <theimaginationwasunbroken@gmail.com>
2020-05-06 17:41:23 +0900

    set frame filter type

--- src/class/FrameFilter.ts
+++ src/class/FrameFilter.ts
@@ -1,9 +1,11 @@
 export default class {
   id: string;
   text: string;
+  type: "" | "normal" | "jump" | "unique" | "throw" | "vsystem" | "special" | "ca";
 
-  constructor(id: string, text: string) {
+  constructor(id: string, text: string, type: "" | "normal" | "jump" | "unique" | "throw" | "vsystem" | "special" | "ca") {
     this.id = id;
     this.text = text;
+    this.type = type;
   }
 }
--- src/data/frame-filter.ts
+++ src/data/frame-filter.ts
@@ -2,13 +2,13 @@ import FrameFilteres from "../class/FrameFilteres";
 import FrameFilter from "../class/FrameFilter";
 
 const frameFilteres = new FrameFilteres();
-frameFilteres.setFrameFilteres(new FrameFilter("0", "指定なし"));
-frameFilteres.setFrameFilteres(new FrameFilter("1", "通常技"));
-frameFilteres.setFrameFilteres(new FrameFilter("2", "ジャンプ技"));
-frameFilteres.setFrameFilteres(new FrameFilter("3", "特殊技"));
-frameFilteres.setFrameFilteres(new FrameFilter("4", "通常投げ"));
-frameFilteres.setFrameFilteres(new FrameFilter("5", "Vシステム"));
-frameFilteres.setFrameFilteres(new FrameFilter("6", "必殺技"));
-frameFilteres.setFrameFilteres(new FrameFilter("7", "クリティカルアーツ"));
+frameFilteres.setFrameFilteres(new FrameFilter("0", "指定なし", ""));
+frameFilteres.setFrameFilteres(new FrameFilter("1", "通常技", "normal"));
+frameFilteres.setFrameFilteres(new FrameFilter("2", "ジャンプ技", "jump"));
+frameFilteres.setFrameFilteres(new FrameFilter("3", "特殊技", "unique"));
+frameFilteres.setFrameFilteres(new FrameFilter("4", "通常投げ", "throw"));
+frameFilteres.setFrameFilteres(new FrameFilter("5", "Vシステム", "vsystem"));
+frameFilteres.setFrameFilteres(new FrameFilter("6", "必殺技", "special"));
+frameFilteres.setFrameFilteres(new FrameFilter("7", "クリティカルアーツ", "ca"));
 
 export { frameFilteres };
