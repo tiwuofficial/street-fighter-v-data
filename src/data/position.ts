fe746ede2aa3337b04079ddea39d146f3a207db9
tiwuofficial <theimaginationwasunbroken@gmail.com>
2020-09-27 22:21:27 +0900

    add 対空

--- src/data/position.ts
+++ src/data/position.ts
@@ -5,5 +5,6 @@ const positions = new Positions();
 
 positions.setPositions(new Position("1", "真ん中"));
 positions.setPositions(new Position("2", "画面端"));
+positions.setPositions(new Position("3", "対空"));
 
 export { positions };
