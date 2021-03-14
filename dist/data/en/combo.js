e98ee6d35577c9bd190e1d04b327dc0099f7ab3d
tiwuofficial <theimaginationwasunbroken@gmail.com>
2021-03-14 17:36:41 +0900

    make en combos

---
+++ dist/data/en/combo.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Combo_1 = require("../../class/Combo");
+const Combos_1 = require("../../class/Combos");
+const character_1 = require("./character");
+const position_1 = require("./position");
+const startStatus_1 = require("./startStatus");
+function getCombosFromLocalStroage() {
+    const combos = new Combos_1.default();
+    const combosFromLS = JSON.parse(localStorage.getItem("combos"));
+    if (combosFromLS) {
+        combosFromLS.forEach(comboFromLS => {
+            const character = character_1.characters.getCharacterById(comboFromLS.characterId);
+            if (character) {
+                const frames = [];
+                comboFromLS.combo.forEach(frameId => {
+                    const frame = character.getFrameById(parseInt(frameId, 10));
+                    if (frame) {
+                        frames.push(frame);
+                    }
+                });
+                let startStatus = startStatus_1.startStatuses.getStartStatusById(comboFromLS.startStatus ? comboFromLS.startStatus : "1");
+                if (!startStatus) {
+                    startStatus = startStatus_1.startStatuses.getStartStatusById("1");
+                }
+                let position = position_1.positions.getPositionById(comboFromLS.position ? comboFromLS.position : "1");
+                if (!position) {
+                    position = position_1.positions.getPositionById("1");
+                }
+                combos.pushCombo(new Combo_1.default(comboFromLS.id, "", character, frames, startStatus, position, comboFromLS.damage, comboFromLS.stun, comboFromLS.title, comboFromLS.memo, comboFromLS.create));
+            }
+        });
+    }
+    return combos;
+}
+exports.getCombosFromLocalStroage = getCombosFromLocalStroage;
+//# sourceMappingURL=combo.js.map
---
+++ src/data/en/combo.ts
@@ -0,0 +1,53 @@
+import Combo from "../../class/Combo";
+import Combos from "../../class/Combos";
+import { characters } from "./character";
+import { positions } from "./position";
+import { startStatuses } from "./startStatus";
+
+export function getCombosFromLocalStroage(): Combos {
+  const combos = new Combos();
+  const combosFromLS = JSON.parse(localStorage.getItem("combos"));
+  if (combosFromLS) {
+    combosFromLS.forEach(comboFromLS => {
+      const character = characters.getCharacterById(comboFromLS.characterId);
+      if (character) {
+        const frames = [];
+        comboFromLS.combo.forEach(frameId => {
+          const frame = character.getFrameById(parseInt(frameId, 10));
+          if (frame) {
+            frames.push(frame);
+          }
+        });
+
+        let startStatus = startStatuses.getStartStatusById(
+          comboFromLS.startStatus ? comboFromLS.startStatus : "1"
+        );
+        if (!startStatus) {
+          startStatus = startStatuses.getStartStatusById("1");
+        }
+
+        let position = positions.getPositionById(comboFromLS.position ? comboFromLS.position : "1");
+        if (!position) {
+          position = positions.getPositionById("1");
+        }
+
+        combos.pushCombo(
+          new Combo(
+            comboFromLS.id,
+            "",
+            character,
+            frames,
+            startStatus,
+            position,
+            comboFromLS.damage,
+            comboFromLS.stun,
+            comboFromLS.title,
+            comboFromLS.memo,
+            comboFromLS.create
+          )
+        );
+      }
+    });
+  }
+  return combos;
+}
